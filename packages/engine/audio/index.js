d58068bfa69db6a80831a99b70947c18668fdcc0
gabriedev <gabrielitodev@gmail.com>
2023-06-08 09:56:28 -0400

    feat(packages/engine): Improve logic and add JSDoc comments to AudioClip class
    Refactored the AudioClip class to enhance its logic and added comprehensive JSDoc comments for
    
    improved documentation and code readability.The improvements include:Restructured the constructor to
    
    store audio objects directly in the  array, simplifying the code.Updated the play method to
    
    check if the audio clip is already playing and restart it from the beginning or play it if
    
    paused.Implemented a new stop method to stop and reset a specific audio clip based on the provided
    
    index.Updated the pause method to utilize the new stop method to pause all currently playing audio
    
    clips.Added JSDoc comments to provide clear and concise explanations of the class, methods,
    
    properties, and parameters.These changes enhance the functionality and maintainability of the
    
    AudioClip class, making it easier to manage and play audio clips while also improving the overall
    
    code quality.

--- packages/engine/audio/index.js
+++ packages/engine/audio/index.js
@@ -1,24 +1,69 @@
+/**
+ * Class representing an AudioClip for managing and playing audio clips.
+ */
 export class AudioClip {
+  /**
+   * Create an instance of the AudioClip class.
+   * @param {...string} resources - Audio resources for the clips.
+   */
   constructor(...resources) {
+    /**
+     * State of the playing clips.
+     * @private
+     * @type {Object.<number, boolean>}
+     */
     this.$playing = {}
+
+    /**
+     * Audio clips.
+     * @private
+     * @type {Array.<{audio: Audio, index: number}>}
+     */
     this.$sounds = resources.map((src, index) => {
+      /**
+       * Audio object.
+       * @type {Audio}
+       */
       const audio = new Audio(src)
-      audio.addEventListener('ended', () => delete this.playing[index])
+
+      audio.addEventListener('ended', () => this.stop(index))
+
       return { audio, index }
     })
   }
 
+  /**
+   * Play a random audio clip.
+   */
   play() {
-    const soundIndex = parseInt(Math.random() * this.$sounds.length)
+    /**
+     * Index of the audio clip to play.
+     * @type {number}
+     */
+    const soundIndex = Math.floor(Math.random() * this.$sounds.length)
     const { audio, index } = this.$sounds[soundIndex]
+
     this.$playing[index] = true
     audio.play()
   }
 
+  /**
+   * Stop and reset a specific audio clip.
+   * @param {number} index - Index of the audio clip to stop.
+   */
+  stop(index) {
+    const audio = this.$sounds[index].audio
+    audio.pause()
+    audio.currentTime = 0
+    delete this.$playing[index]
+  }
+
+  /**
+   * Pause all currently playing audio clips.
+   */
   pause() {
     for (const index in this.$playing) {
-      this.$sounds[index].audio.pause()
-      delete this.$playing[index]
+      this.stop(index)
     }
   }
 }
