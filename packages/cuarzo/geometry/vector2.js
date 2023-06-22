d46850382fc10bc37f593ee8a3f7307321fd45ec
gabriedev <gabrielitodev@gmail.com>
2023-06-21 20:51:06 -0400

    feat(packages/cuarzo): Enhance Vector2 module with additional methods
    The Vector2 module has been enhanced with additional methods to expand its functionality and provide
    
    more utility for working with 2D vectors. The new methods include:
    
    - : Calculates the
    
    dot product between the current vector and another given vector.
    
    - : Computes the
    
    distance between the current vector and another given vector.
    
    - : Determines the
    
    angle in radians between the current vector and another given vector.
    
    These new methods allow for
    
    more advanced vector calculations, such as determining the similarity or alignment between vectors,
    
    measuring the distance between vectors in space, and calculating the angle between vectors.
    
    These
    
    enhancements further empower developers to perform various operations and calculations on 2D vectors
    
    efficiently and accurately, enabling the creation of more robust and dynamic applications.
    
    BREAKING CHANGES:
    
    There are no breaking changes in the recent enhancements made to the Vector2 module. All existing
    
    methods and their behavior remain unchanged, and the new methods are added as additional
    
    functionality. Therefore, there should be no disruptions or compatibility issues when using the
    
    updated version of the Vector2 module in existing projects.

--- packages/cuarzo/geometry/vector2.js
+++ packages/cuarzo/geometry/vector2.js
@@ -1,32 +1,92 @@
+/**
+ * Class representing a 2D vector.
+ */
 export class Vector2 {
+  /**
+   * Creates a new Vector2 object.
+   * @param {number} x - The x value of the vector. Defaults to 0.
+   * @param {number} y - The y value of the vector. Defaults to 0.
+   */
   constructor(x = 0, y = 0) {
     this.x = x
     this.y = y
   }
 
+  /**
+   * Subtracts a given vector from this vector.
+   * @param {Vector2} vector - The vector to subtract.
+   */
   sub(vector) {
     this.x = this.x - vector.x
     this.y = this.y - vector.y
   }
 
+  /**
+   * Multiplies this vector by a given number.
+   * @param {number} n - The number to multiply.
+   */
   mult(n) {
     this.x = this.x * n
     this.y = this.y * n
   }
 
+  /**
+   * Divides this vector by a given number.
+   * @param {number} n - The number to divide.
+   */
   div(n) {
     this.x = this.x / n
     this.y = this.y / n
   }
 
+  /**
+   * Calculates the magnitude or length of this vector.
+   * @returns {number} The magnitude of the vector.
+   */
   mag() {
     return Math.sqrt(this.x * this.x + this.y * this.y)
   }
 
+  /**
+   * Normalizes this vector, converting it into a unit vector.
+   * If the magnitude of the vector is greater than 0, it divides the vector by its magnitude.
+   */
   normalized() {
     const mag = this.mag()
     if (mag > 0) {
       this.div(mag)
     }
+  }
+
+  /**
+   * Calculates the dot product between this vector and another given vector.
+   * @param {Vector2} vector - The vector to calculate the dot product with.
+   * @returns {number} The dot product between the two vectors.
+   */
+  dot(vector) {
+    return this.x * vector.x + this.y * vector.y
+  }
+
+  /**
+   * Calculates the distance between this vector and another given vector.
+   * @param {Vector2} vector - The vector to calculate the distance to.
+   * @returns {number} The distance between the two vectors.
+   */
+  distance(vector) {
+    const dx = this.x - vector.x
+    const dy = this.y - vector.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+
+  /**
+   * Calculates the angle between this vector and another given vector.
+   * @param {Vector2} vector - The vector to calculate the angle to.
+   * @returns {number} The angle in radians between the two vectors.
+   */
+  angle(vector) {
+    const dot = this.dot(vector)
+    const magA = this.mag()
+    const magB = vector.mag()
+    return Math.acos(dot / (magA * magB))
   }
 }
