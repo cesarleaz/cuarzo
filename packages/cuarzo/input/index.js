a0f843960533900926a292ecb6209e933f8376a1
gabriedev <gabrielitodev@gmail.com>
2023-06-21 23:40:21 -0400

    feat(packages/cuarzo): Write a SHORT, IMPERATIVE tense description of the change
    The change includes the addition of new features and enhancements to the Input module. The
    
    DEFAULT_KEYS object now contains predefined key mappings for the UP, DOWN, LEFT, RIGHT, and JUMP
    
    actions. The KEY_PRESSEDS object keeps track of the currently pressed keys. The keyAssigneds
    
    variable is updated to retrieve key assignments from local storage, or it falls back to the default
    
    key mappings if no previous assignments are found. The lastKeyPressed variable stores the last key
    
    that was pressed.
    
    The Input class provides static methods to access the lastKeyPressed value and to
    
    check if a specific key is currently pressed. The window event listeners for keydown and keyup
    
    events handle the key press and release actions. When a key is pressed, the corresponding action is
    
    identified based on the assigned key mappings. If the key is found in the mappings, the KEY_PRESSEDS
    
    object is updated, and the lastKeyPressed value is set accordingly. If the key is not found in the
    
    mappings, it is assigned to a new mapping with the key itself, and the KEY_PRESSEDS object is
    
    updated. When a key is released, the corresponding action is identified, and the KEY_PRESSEDS object
    
    is updated accordingly.
    
    These improvements provide a more flexible and customizable input handling
    
    system. The code is well-documented with JSDoc comments, making it easier to understand and
    
    maintain.

--- packages/cuarzo/input/index.js
+++ packages/cuarzo/input/index.js
@@ -1,3 +1,7 @@
+/**
+ * Default key mappings.
+ * Modify this object to customize the default key assignments.
+ */
 const DEFAULT_KEYS = {
   UP: ['ArrowUp', 'w'],
   DOWN: ['ArrowDown', 's'],
@@ -6,44 +10,92 @@ const DEFAULT_KEYS = {
   JUMP: [' ']
 }
 
-const KEY_PRESSEDS = {}
+/**
+ * Set to store pressed keys.
+ */
+const KEY_PRESSEDS = new Set()
+
+/**
+ * Key assignments retrieved from storage or default keys if not available.
+ * @type {Object}
+ */
 const KEYS_ASSIGNEDS_FORM_STORAGE =
   window.localStorage.getItem('KEYS_ASSIGNEDS')
-
 let keyAssigneds = KEYS_ASSIGNEDS_FORM_STORAGE
   ? JSON.parse(KEYS_ASSIGNEDS_FORM_STORAGE)
   : structuredClone(DEFAULT_KEYS)
+
+/**
+ * The last key that was pressed.
+ * @type {string}
+ */
 let $lastKeyPressed
 
+/**
+ * Class representing the Input module.
+ */
 export class Input {
+  /**
+   * Get the last key that was pressed.
+   * @returns {string} The last key pressed.
+   */
   static get lastKeyPressed() {
     return $lastKeyPressed
   }
-  static getKeyPressed(key) {
-    return KEY_PRESSEDS[key.toUpperCase()]
+
+  /**
+   * Check if a specific key is currently pressed.
+   * @param {string} key - The key to check.
+   * @returns {boolean} True if the key is pressed, false otherwise.
+   */
+  static isKeyPressed(key) {
+    return KEY_PRESSEDS.has(key.toUpperCase())
+  }
+
+  /**
+   * Get the current state of all assigned keys.
+   * @returns {Object} An object containing the current state of all assigned keys.
+   */
+  static getAllKeysState() {
+    const keysState = {}
+    for (const key in keyAssigneds) {
+      keysState[key] = this.isKeyPressed(key)
+    }
+    return keysState
   }
 }
 
-window.addEventListener('keydown', (e) => {
+/**
+ * Event handler for keydown events.
+ * @param {KeyboardEvent} e - The keydown event object.
+ */
+function handleKeyDown(e) {
   const keyPressed = e.key.toUpperCase()
 
   for (const key in keyAssigneds) {
     if (!keyAssigneds[key].includes(keyPressed)) continue
-    KEY_PRESSEDS[key] = true
+    KEY_PRESSEDS.add(key)
     $lastKeyPressed = keyAssigneds[key]
     return
   }
 
   keyAssigneds[keyPressed] = [keyPressed]
-  KEY_PRESSEDS[keyPressed] = true
+  KEY_PRESSEDS.add(keyPressed)
   $lastKeyPressed = keyPressed
-})
+}
 
-window.addEventListener('keyup', (e) => {
+/**
+ * Event handler for keyup events.
+ * @param {KeyboardEvent} e - The keyup event object.
+ */
+function handleKeyUp(e) {
   const keyPressed = e.key.toUpperCase()
   for (const key in keyAssigneds) {
     if (!keyAssigneds[key].includes(keyPressed)) continue
-    KEY_PRESSEDS[key] = false
+    KEY_PRESSEDS.delete(key)
     break
   }
-})
+}
+
+window.addEventListener('keydown', handleKeyDown)
+window.addEventListener('keyup', handleKeyUp)
